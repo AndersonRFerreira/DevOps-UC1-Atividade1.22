a40ab6fbbee292c85da45af35251906c54670f57
andersonRFerreira <andersonn.reis90@gmail.com>
2025-03-25 21:07:27 -0300

    subindo arquivos para analise

---
+++ analise_vendas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Carregar dados de vendas 
+
+dados = pd.read_csv("data/vendas.csv")
+
+#calcular total de vendas
+total_vendas = dados["valor"].sum()
+print(f"Total de Vendas: R${total_vendas: .2f}")
